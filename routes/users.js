2e840204e3abc9c473cc2d001c86df8aea444814
CooLNuanfeng <1634898698@qq.com>
2018-09-08 09:50:27 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,14 @@ const router = require('koa-router')()
 
 router.prefix('/users')
 
+router.get('/login', function (ctx, next) {
+  ctx.body = 'login!'
+})
+
+router.get('/register', function (ctx, next) {
+  ctx.body = 'register!'
+})
+
 router.get('/', function (ctx, next) {
   ctx.body = 'this is a users response!'
 })
